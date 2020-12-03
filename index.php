5799d17f350cbd47350ed553493d641ed83119da
yann-app <yannochh@gmail.com>
2020-12-03 17:52:40 +0100

    version site 1

---
+++ index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-887VDTZSXV"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-887VDTZSXV');
+    </script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css.css">
+    <title>Micro Services Informatique</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+
+    <div class="col-3 col-md-2 col-sm-4">
+        <img class="img-fluid" src="images/Logo.jpg" alt="Logo">
+    </div>
+<h1 class="col-12 col-md-10 col-sm-4">Micro Services Informatique</h1>
+
+</div>
+</div>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-uppercase" href="#">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="#">Presentation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="#">Tarif</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="#">Contact</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
+
+
+g
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="1800">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="img-fluid" src="images/pc.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="images/pc2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="images/pc3.jpg" alt="Third slide">
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+    <div class="col-12 col-md-12 col-sm-12">
+        <p class="text-center">
+            Je vous propose mes services pour le dépannage et la réparation de votre ordinateur portable ou fixe, ainsi que la vente de pièces détachées pour pc portable ou fixe.<br>
+
+            -Recherche de votre pièce détachée au meilleur prix<br>
+            -Remplacement dalle d'écran et clavier pc portable<br>
+            -Blocages au démarrage<br>
+            -Réinstallation système d'exploitation<br>
+            -Optimisation<br>
+            -Conseils utilisation de votre ordinateur<br>
+            -Nettoyage programmes inutiles<br>
+            -Formatage<br>
+            -Virus<br>
+            -Installation et configuration logiciels
+        </p>
+    </div>
+</div>
+</div>
+
+<footer>
+
+    <h3>Contact : 06.70.29.27.24</h3>
+
+</footer>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+-->
+
+</body>
+</html>
